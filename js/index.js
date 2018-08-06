db4b43456d008c9c676248aa76601897abb3a90a
Marcus <mwingate91@gmail.com>
2018-08-06 11:14:18 -0400

    Add projects to page in grid.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+Amplitude.init({
+    "bindings": {
+        37: 'prev',
+        39: 'next',
+        32: 'play_pause'
+    },
+    "songs": [
+        {
+            "name": "Risin' High (feat Raashan Ahmad)",
+            "artist": "Ancient Astronauts",
+            "album": "We Are to Answer",
+            "url": "https://soundcloud.com/big-kahuna-og/big-kahuna-og-sky-chryme",
+            "cover_art_url": "/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"
+        }
+    ]
+});
